d04b3a8855fdc322516785ca02ee2870f100ea4f
jdelles <james.delles@gmail.com>
2024-01-22 16:12:52 -0500

    Chapter 7 - Updated Dijkstra's algo with better understanding

--- src/main/kotlin/Chapter7/DijkstrasAlgorithm.kt
+++ src/main/kotlin/Chapter7/DijkstrasAlgorithm.kt
@@ -1,8 +1,6 @@
 package Chapter7
 
 fun main() {
-    val processed = mutableListOf<String>()
-
     val graph = mapOf(
         "start" to mapOf("a" to 6.0, "b" to 2.0),
         "a" to mapOf("fin" to 1.0),
@@ -10,52 +8,41 @@ fun main() {
         "fin" to HashMap(),
     )
 
-    val costs = mutableMapOf(
-        "a" to 6.0,
-        "b" to 2.0,
-        "fin" to Double.MAX_VALUE,
-    )
+    println(dijkstra(graph, "start"))
 
-    val parents = mutableMapOf(
-        "a" to "start",
-        "b" to "start",
-        "fin" to null,
-    )
-
-    var node = findLowestCostNode(costs, processed)
-    while (node != null) {
-        val cost = costs[node]!!
-        val neighbors = graph[node]!!
-
-        for (n in neighbors.keys) {
-            val newCost = cost.plus(neighbors[n]!!)
-            if (costs[n]!! > newCost) {
-                costs[n] = newCost
-                parents[n] = node
-            }
-        }
-
-        processed.add(node)
-        node = findLowestCostNode(costs, processed)
-    }
-    println("Cost from the start to each node: ")
-    println(costs) // Expected: 5, 2, 6
 }
 
-fun findLowestCostNode(
-    costs: Map<String, Double>,
-    processed: List<String>,
-): String? {
-    var lowestCost = Double.MAX_VALUE
-    var lowestCostNode: String? = null
+fun dijkstra(graph: Map<String, Map<String, Double>>, startingNode: String): Map<String, Double> {
+    val shortestDistances = mutableMapOf<String, Double>()
+    val processed = mutableListOf<String>()
+    val parents = mutableMapOf<String, String>()
 
-    for (node in costs) {
-        val cost = node.value
-        if (cost < lowestCost && !processed.contains(node.key)) {
-            lowestCost = cost
-            lowestCostNode = node.key
+    graph.keys.forEach { node ->
+        shortestDistances[node] = if (node == startingNode) 0.0 else Double.MAX_VALUE
+    }
+
+    while (true) {
+        val currentNode = shortestDistances
+            .filter { !processed.contains(it.key) }
+            .minByOrNull { it.value }?.key
+            ?: break
+
+        processed.add(currentNode)
+
+        graph[currentNode]?.forEach { (neighbor, distance) ->
+            val newDistance = shortestDistances[currentNode]!! + distance
+            if (newDistance < shortestDistances[neighbor]!!) {
+                shortestDistances[neighbor] = newDistance
+                parents[neighbor] = currentNode
+            }
         }
     }
 
-    return lowestCostNode
+    val shortestPaths = shortestDistances.keys.associateWith { node ->
+        generateSequence(node) { parents[it] }.toList().asReversed()
+    }
+
+    println(shortestPaths)
+
+    return shortestDistances
 }
